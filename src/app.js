4531e9971001c69a11cbd319ef7e21011d438b66
luchanso <oleg@loutchansky.com>
2016-10-13 21:05:29 +0300

    Add app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
-Engine.game = new Phaser.Game(Engine.maxWidth, Engine.maxHeight, Phaser.AUTO);
+Engine.game = new Phaser.Game(Engine.maxWidth, Engine.maxHeight, Phaser.AUTO)
 
 window.onresize = () => {
-  Engine.game.scale.setGameSize(window.innerWidth, window.innerHeight);
-};
+  Engine.game.scale.setGameSize(window.innerWidth, window.innerHeight)
+}
 
-Engine.game.state.add('Boot', Engine.Boot);
-Engine.game.state.add('Menu', Engine.Menu);
-Engine.game.state.add('Game', Engine.Game);
-Engine.game.state.add('Loader', Engine.Loader);
-Engine.game.state.add('Lvlpick', Engine.Lvlpick);
-Engine.game.state.add('Settings', Engine.Settings);
-// Engine.game.state.add('Leaderboard', Engine.Leaderboard);
+Engine.game.state.add('Boot', Engine.Boot)
+Engine.game.state.add('Game', Engine.Game)
+Engine.game.state.add('Loader', Engine.Loader)
 
-Engine.game.state.start('Boot');
+Engine.game.state.start('Boot')

b45b883550b764bc479e66d78c020679540609cc
luchanso <oleg@loutchansky.com>
2016-10-13 21:06:24 +0300

    Add background class

---
+++ src/game/actors/background.js
@@ -0,0 +1,18 @@
+class Background extends Phaser.TileSprite {
+  constructor(game, x, y, name, speed) {
+    super(game, x, y, 1024, 1024, name)
+
+    this.tileScale.setTo(this.height / this.game.height)
+    this.fixedToCamera = true
+    this.width = this.game.width
+    // this.height = this.game.height
+
+    this._speed = speed
+  }
+
+  update() {
+    this.tilePosition.x += this._speed
+  }
+}
+
+Engine.Background = Background

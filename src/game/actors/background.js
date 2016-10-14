a7dece871beb95ec80f83bfa2c2aaab27124aa6f
Luchanso <luchansodev@gmail.com>
2016-10-14 21:26:22 +0300

    Fix sclae bacground layers

--- src/game/actors/background.js
+++ src/game/actors/background.js
@@ -2,7 +2,7 @@ class Background extends Phaser.TileSprite {
   constructor(game, x, y, name, speed) {
     super(game, x, y, 1024, 1024, name)
 
-    this.tileScale.setTo(this.height / this.game.height)
+    this.tileScale.setTo(this.game.height / this.height)
     this.fixedToCamera = true
     this.width = this.game.width
     // this.height = this.game.height

c943ebacd7576516caa30aabae2ede4b0a98e34c
Luchanso <luchansodev@gmail.com>
2016-10-14 21:27:35 +0300

    Add ground generator

--- src/game/game.js
+++ src/game/game.js
@@ -21,18 +21,23 @@ class Game extends Phaser.State {
     this.world.setBounds(0, 0, Number.MAX_VALUE, this.game.height);
 
     this.createBackground()
-    this.createGrounds()
+
+    this.grounds = this.add.group()
+
+    this.createStartGround()
 
     window.bunny = this.bunny = new Engine.Bunny(this.game, 150, 150, 'bunny1')
-    this.bunny.body.velocity.setTo(500, 0)
     this.add.existing(this.bunny)
 
     this.configurateCamera()
     this.addControl()
+
+    this._groundStep = 0
   }
 
   update() {
-      this.physics.arcade.collide(this.bunny, this.grounds)
+    this.physics.arcade.collide(this.bunny, this.grounds)
+    this.updateGrounds()
   }
 
   render() {
@@ -41,44 +46,132 @@ class Game extends Phaser.State {
     // })
   }
 
+  updateGrounds() {
+    let step = Math.round(this.bunny.x / 150)
+
+    if (step % 2 === 0 && step !== this._groundStep) {
+      this._groundStep = step
+      this.generateGrounds()
+    }
+  }
+
   addControl() {
     let hotkey = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
     hotkey.onDown.add(this.jump, this)
   }
 
   jump() {
-    this.bunny.jump()
+    if (this.bunny.data.running) {
+      this.bunny.jump()
+    }
+    else {
+      this.bunny.run()
+      this.bunny.jump()
+    }
+  }
+
+  createStartGround() {
+    const marginBottom = 100
+    const x = 100
+    const y = this.game.height - marginBottom
+    const type = Ground.type.GRASS
+    const small = false
+    const broken = false
+
+    let startGround = new Engine.Ground(
+      this.game,
+      x,
+      y,
+      type,
+      small,
+      broken,
+    )
+
+    this.grounds.add(startGround)
+  }
+
+  generateGrounds() {
+    let margin = 0
+
+    if (this.bunny.x < this.game.width) {
+
+    }
+
+    const SPLIT_VERTICAL = 5
+    const GRID_HEIGHT = this.game.height / SPLIT_VERTICAL
+
+    for (let i = 1; i < SPLIT_VERTICAL; i++) {
+      if (this.rnd.pick[true, false]) continue
+
+      const x = this.bunny.x + (this.game.width - 100) + this.rnd.between(-50, 50)
+      const y = GRID_HEIGHT * i + this.rnd.between(-50, 50)
+
+      this.activateRandomGround(x, y)
+    }
+
+  }
+
+  activateRandomGround(x, y) {
+    const marginBottom = 100
+
+    const types = Object.keys(Engine.Ground.type).map(val => {
+      return Engine.Ground.type[val]
+    })
+    const type = this.rnd.pick(types)
+    const small = this.rnd.pick([true, false])
+    const broken = this.rnd.pick([true, false])
+
+    let ground = this.grounds.getFirstDead()
+    if (ground == null) {
+      ground = new Engine.Ground(
+        this.game,
+        x,
+        y,
+        type,
+        small,
+        broken,
+      )
+      this.grounds.add(ground)
+    } else {
+      ground.reset(x, y, type, small, broken)
+    }
+
+    return ground
   }
 
   createGrounds() {
-    this.grounds = this.add.group()
+    window.d = this.grounds = this.add.group()
     const types = Object.keys(Engine.Ground.type).map(val => {
       return Engine.Ground.type[val]
     })
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 15; i++) {
       let groundType = this.rnd.pick(types)
       let ground = new Engine.Ground(
         this.game,
-        400 + i * 400,
+        400 + i * 250,
         450,
         groundType,
         false,
         this.rnd.pick([true, false])
       )
 
+      // ground.autoCull = true
+      // ground.outOfCameraBoundsKill = true
+
       this.grounds.add(ground)
     }
   }
 
   configurateCamera() {
+    this.camera.roundPx = false
     this.camera.follow(this.bunny)
-    this.camera.deadzone = new Phaser.Rectangle(0, 0, 100, 0)
+    this.camera.deadzone = new Phaser.Rectangle(100, 0, 0, 0)
   }
 
   createBackground() {
-    this.add.existing(new Engine.Background(this.game, 0, 0, 'layer2', -1))
-    // this.add.existing(new Engine.Background(this.game, 0, 0, 'layer3', -2))
+    // this.add.existing(new Engine.Background(this.game, 0, 0, 'layer2', -1))
+    this.add.existing(new Engine.Background(this.game, 0, 0, 'layer3', -2))
     // this.add.existing(new Engine.Background(this.game, 0, 0, 'layer4', -3))
   }
 }

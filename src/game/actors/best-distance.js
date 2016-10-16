2aa2b4a06229b43543c205e57242db7c20b230fc
Luchanso <luchansodev@gmail.com>
2016-10-16 21:48:51 +0300

    Add best distance class

---
+++ src/game/actors/best-distance.js
@@ -0,0 +1,32 @@
+class BestDistance {
+  constructor(game) {
+    this.score = Engine.Service.get('Score')
+
+    this.game = game
+    this.x = this.score.bestDistance * Engine.Score.MULTIPER_DISTANCE
+
+    this.createLabel()
+    this.createStripe()
+  }
+
+  createLabel() {
+    const style = {
+      fill: 'white',
+      font: '26px Arial'
+    }
+    const marginLeft = 10
+    const marginTop = 25
+
+    this.label = this.game.add.text(this.x + marginLeft, 0, `Best ${this.score.bestDistance} m.`, style)
+    this.label.update = () => {
+      this.label.y = this.game.camera.y + marginTop
+    }
+  }
+
+  createStripe() {
+    this.stripe = new Engine.BestDistanceStripe(this.game, this.x)
+    this.game.add.existing(this.stripe)
+  }
+}
+
+Engine.BestDistance = BestDistance

01399ecedae896a0aa2001d7dd7903dbafa0e7d0
Luchanso <luchansodev@gmail.com>
2016-10-17 20:44:56 +0300

    Add spike class

---
+++ src/game/actors/spike.js
@@ -0,0 +1,14 @@
+class Spike extends Phaser.Sprite {
+  constructor(game, x, y) {
+    super(game, x, y, Engine.spritesheet, 'spikes_top.png')
+
+    this.autoCull = true
+
+    this.width *= 0.35
+    this.height *= 0.35
+
+    this.tint = 0x777777
+  }
+}
+
+Engine.Spike = Spike

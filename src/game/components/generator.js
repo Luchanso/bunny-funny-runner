c2cc58e9d2bc0b613dbadcb989ce2d0499294a8c
Luchanso <luchansodev@gmail.com>
2016-10-18 19:20:21 +0300

    Add Generator component

---
+++ src/game/components/generator.js
@@ -0,0 +1,24 @@
+class Generator extends Phaser.Group {
+  constructor(game) {
+    super(game)
+  }
+
+  start() {
+
+  }
+
+  update() {
+
+  }
+
+  generate() {
+
+  }
+
+  stop() {
+
+  }
+}
+
+Engine.Component = {}
+Engine.Component.Generator = Generator

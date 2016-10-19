bb49e8e6b9853c1e617398d19a5473a989bf736c
Luchanso <luchansodev@gmail.com>
2016-10-20 02:17:25 +0300

    Add nominals generator

---
+++ src/game/components/generators/nominals.js
@@ -0,0 +1,20 @@
+class NominalGenerator extends Generator {
+  constructor(game, bunny) {
+    super(game, bunny)
+  }
+
+  generate(x, y, nominal) {
+    let item = this.getFirstDead()
+
+    if (item == null) {
+      item = new Engine.Nominal(this.game, x, y, nominal)
+      this.add(item)
+    } else {
+      item.reset(x, y, nominal)
+    }
+
+    return item
+  }
+}
+
+Engine.Component.NominalGenerator = NominalGenerator

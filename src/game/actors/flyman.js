50c6ff3b50fc15bae1dbaefcbfd12d2414ee09a8
Luchanso <luchansodev@gmail.com>
2016-10-20 22:37:58 +0300

    FlyMan class

---
+++ src/game/actors/flyman.js
@@ -0,0 +1,56 @@
+class FlyMan extends Engine.Enemy {
+  constructor(game, x, y) {
+    super(game, x, y, 'flyMan_still_stand.png')
+
+    this.width *= 0.35
+    this.height *= 0.35
+
+    this.game.physics.arcade.enable([ this ])
+
+    this.verticaleTween = this.addVerticaleMove()
+  }
+
+  addVerticaleMove() {
+    const distance = this.game.rnd.between(50, 125)
+    const time = this.game.rnd.between(850, 1250)
+
+    return this.game.add.tween(this)
+      .to({
+        y: this.y + distance
+      }, time)
+      .to({
+        y: this.y
+      }, time)
+      .to({
+        y: this.y - distance
+      }, time)
+      .to({
+        y: this.y
+      }, time)
+      .loop()
+      .start()
+  }
+
+  reset(x, y) {
+    super.reset(x, y)
+
+    this.addVerticaleMove()
+  }
+
+  die() {
+    const impulse = 400
+
+    this.game.tweens.removeFrom(this)
+    this.body.angularVelocity = this.game.rnd.between(100, 600)
+    this.body.velocity.y = -impulse
+    this.body.gravity.setTo(200, 2000)
+  }
+
+  kill() {
+    this.game.tweens.removeFrom(this)
+
+    super.kill()
+  }
+}
+
+Engine.FlyMan = FlyMan

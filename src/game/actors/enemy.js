d0ff7f0e2fb0128821cc86942a2248e3fe6a9bff
Luchanso <luchansodev@gmail.com>
2016-10-21 23:18:47 +0300

    Refactor all enemies

--- src/game/actors/enemy.js
+++ src/game/actors/enemy.js
@@ -1,6 +1,12 @@
 class Enemy extends Phaser.Sprite {
   constructor(game, x, y, name) {
     super(game, x, y, Engine.spritesheet, name)
+
+    this.game.physics.arcade.enable([ this ])
+    this.body.immovable = true
+
+    this.width *= Engine.scaleRatio
+    this.height *= Engine.scaleRatio
   }
 
   die() {

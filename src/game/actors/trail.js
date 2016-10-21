7899a03543f60834d82ac3f474c1b987f4d11a95
Luchanso <luchansodev@gmail.com>
2016-10-21 23:20:28 +0300

    Add particle bounce to trail

--- src/game/actors/trail.js
+++ src/game/actors/trail.js
@@ -4,6 +4,7 @@ class Trail extends Phaser.Particles.Arcade.Emitter {
 
     this.makeParticles('particles', 0, maxParticles, true)
     this.lifespan = 500
+    this.bounce.setTo(1)
 
     this._particlesEmit = 3
     this._delayEmit = 35
@@ -16,13 +17,13 @@ class Trail extends Phaser.Particles.Arcade.Emitter {
 
   emit() {
     for (let i = 0; i < this._particlesEmit; i++) {
-      const particleFram = this.game.rnd.between(0, 4)
+      const particleFrame = this.game.rnd.between(0, 4)
 
       this.emitParticle(
         this._follow.x,
         this._follow.y + this._follow.height / 1.1,
         'particles',
-        particleFram
+        particleFrame
       )
     }
   }

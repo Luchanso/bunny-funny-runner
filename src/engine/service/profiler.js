7ce930a4a282d44104c2022371591fb4f517c724
Luchanso <luchansodev@gmail.com>
2016-10-22 20:40:54 +0300

    Add profiler class

---
+++ src/engine/service/profiler.js
@@ -0,0 +1,25 @@
+class Profiler {
+  constructor() {
+    this.array = []
+  }
+
+  start(name) {
+    this.array[name] = {
+      start: Date.now()
+    }
+  }
+
+  stop(name) {
+    let data = this.array[name]
+
+    data.stop = Date.now()
+    data.time = (data.stop - data.start)
+  }
+
+  time(name) {
+    if (this.array[name] == null) return undefined
+    else                          return this.array[name].time
+  }
+}
+
+Engine.Profiler = Profiler

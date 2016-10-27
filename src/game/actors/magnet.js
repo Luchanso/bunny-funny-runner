ba59050dc77700507e9baeb1878ecd14aad6777e
Luchanso <luchansodev@gmail.com>
2016-10-27 20:47:15 +0300

    Add show, hide function, and fix memory leak

--- src/game/actors/magnet.js
+++ src/game/actors/magnet.js
@@ -5,8 +5,10 @@ class MagnetEffect extends Phaser.Graphics {
     this.waves = []
 
     this.waveGenerator = this.game.time.create()
-    this.waveGenerator.loop(200, this.addWave, this)
-    this.waveGenerator.start()
+    this.waveGenerator.loop(900, this.addWave, this)
+
+
+    this.alpha = 0
   }
 
   update() {
@@ -16,31 +18,59 @@ class MagnetEffect extends Phaser.Graphics {
   draw() {
     this.clear()
 
-    for (let wave of this.waves) {
+    for (let i = 0; i < this.waves.length; i++) {
+      let wave = this.waves[i]
+
       if (Date.now() - wave.startTime > 2000) {
+        this.waves.splice(i, 1)
         continue
       }
-      this.lineStyle(wave.width, 0xff7a00, wave.alpha)
+
+      this.lineStyle(wave.width, 0xff8000, wave.alpha)
       this.drawCircle(0, 0, wave.radius)
     }
   }
 
+  show() {
+    this.fade(1)
+
+    if (!this.waveGenerator.paused && !this.waveGenerator.running) {
+      this.waveGenerator.start()
+    } else {
+      this.waveGenerator.resume()
+    }
+  }
+
+  hide() {
+    this.fade(0)
+
+    this.waveGenerator.pause()
+  }
+
+  fade(alpha) {
+    const animationTime = 150
+
+    this.game.add.tween(this)
+      .to({
+        alpha
+      }, animationTime)
+      .start()
+  }
+
   addWave() {
     let wave = {
-      radius: Engine.magnetDistace * 2,
-      width: 1,
-      alpha: 0,
+      radius: 0,
+      width: 15,
+      alpha: 0.8,
       startTime: Date.now()
     }
 
     let tween = this.game.add.tween(wave)
       .to({
-        radius: Engine.magnetDistace / 2,
-        alpha: 0.4
-      }, 1500)
-      .to({
+        width: 1,
+        radius: Engine.magnetDistace,
         alpha: 0
-      }, 500)
+      }, 750)
       .start()
 
     this.waves.push(wave)

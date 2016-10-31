8137f8a6a82b3d2b91757a2ab9024caf404bf5b9
Luchanso <luchansodev@gmail.com>
2016-11-01 00:21:30 +0300

    Add fire for bunny

---
+++ src/game/actors/fire.js
@@ -0,0 +1,50 @@
+class Fire extends Phaser.Sprite {
+  constructor(game) {
+    super(game, 0, 0, Engine.spritesheet, 'flame.png')
+
+    this.width /= Engine.scaleRatio
+    this.height /= Engine.scaleRatio
+
+    this.addAnimation()
+  }
+
+  hide() {
+    this.fade(0)
+  }
+
+  show() {
+    this.fade(1)
+  }
+
+  fade(alpha) {
+    let animation = 100
+
+    this.game.add.tween(this)
+      .to({
+        alpha
+      }, animation)
+      .start()
+  }
+
+  addAnimation() {
+    let animation = 100
+
+    this.game.add.tween(this)
+      .to({
+        width: this.width * 0.75,
+        height: this.height * 0.9
+      }, animation)
+      .to({
+        width: this.width * 1.23,
+        height: this.height * 1.1
+      }, animation)
+      .to({
+        width: this.width,
+        height: this.height
+      }, animation)
+      .loop(-1)
+      .start()
+  }
+}
+
+Engine.Fire = Fire

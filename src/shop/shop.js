89094261e699344268c0ef2964ee94dab0c2e949
Luchanso <luchansodev@gmail.com>
2016-11-04 21:22:26 +0300

    Add btnshop

---
+++ src/shop/shop.js
@@ -0,0 +1,37 @@
+class Shop extends Phaser.State {
+  constructor() {
+    super()
+  }
+
+  create() {
+    this.game.stage.backgroundColor = 0xBF360C
+
+    this.createHeadLabel()
+  }
+
+  createHeadLabel() {
+    const margin = 100
+    const animation = 400
+    const text = 'BUY MORE COINS 💰';
+    const style = {
+      font: '41px Open Sans',
+      fill: 'white'
+    }
+
+    this.headLabel = this.add.text(
+      this.game.width / 2,
+      margin,
+      text,
+      style
+    )
+    this.headLabel.anchor.setTo(0.5)
+    this.headLabel.alpha = 0
+    this.add.tween(this.headLabel)
+      .to({
+        alpha: 1
+      }, animation)
+      .start()
+  }
+}
+
+Engine.Shop = Shop

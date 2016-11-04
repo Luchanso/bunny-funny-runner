65f69c1876421bd3366fb341c573ee02d8adb14c
Luchanso <luchansodev@gmail.com>
2016-11-04 19:26:42 +0300

    Fix displayContainer in phaser

---
+++ src/fix.js
@@ -0,0 +1,29 @@
+PIXI.DisplayObjectContainer.prototype.removeChildren = function (beginIndex, endIndex) {
+
+    if (beginIndex === undefined) { beginIndex = 0; }
+    if (endIndex === undefined) { endIndex = this.children.length; }
+
+    var range = endIndex - beginIndex;
+
+    if (range > 0 && range <= endIndex)
+    {
+        var removed = this.children.splice(beginIndex, range);
+
+        for (var i = 0; i < removed.length; i++)
+        {
+            var child = removed[i];
+            child.parent = undefined;
+        }
+
+        return removed;
+    }
+    else if (range === 0 && this.children.length === 0)
+    {
+        return [];
+    }
+    else
+    {
+        throw new Error( 'removeChildren: Range Error, numeric values are outside the acceptable range' );
+    }
+
+};

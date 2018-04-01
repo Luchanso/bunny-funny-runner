d033d43e395c27b2243d34014e86bc0a4f56e654
Luchansky Oleg <luchansodev@gmail.com>
2018-04-01 21:36:04 +0300

    feat(runVkAd): split code

--- src/components/VkAd/runVkAd.js
+++ src/components/VkAd/runVkAd.js
@@ -3,32 +3,35 @@ import debug from 'debug';
 
 const vkLog = debug('vk');
 
-const vkAdsInit = (id, adParams) => {
+const vkAdInit = (id, adParams) => {
   vkLog('call vkAdsInit()', id, {}, adParams);
   VK.Widgets.Ads(`vk_ads_${id}`, {}, adParams);
 };
 
+const vkAdLoad = (id, adParams) => {
+  vkLog('load vk scripts');
+  if (!window.vkAsyncInitCallbacks) window.vkAsyncInitCallbacks = [];
+
+  vkAsyncInitCallbacks.push(() => vkAdInit(id, adParams));
+  const protocol = window.location.protocol === 'https:' ? 'https:' : 'http:';
+  const adsElem = document.getElementById(`vk_ads_${id}`);
+  const scriptElem = document.createElement('script');
+  scriptElem.type = 'text/javascript';
+  scriptElem.async = true;
+  scriptElem.src = `${protocol}//vk.com/js/api/openapi.js?152`;
+  adsElem.parentNode.insertBefore(scriptElem, adsElem.nextSibling);
+};
+
 const run = id => {
-  const adsParams = {
+  const adParams = {
     ad_unit_id: id,
     ad_unit_hash: 'cd03259b32fb224628362b0fdabb4254'
   };
 
   if (window.VK && VK.Widgets) {
-    vkAdsInit(id, adsParams);
+    vkAdInit(id, adParams);
   } else {
-    vkLog('load vk scripts');
-
-    if (!window.vkAsyncInitCallbacks) window.vkAsyncInitCallbacks = [];
-
-    vkAsyncInitCallbacks.push(vkAdsInit);
-    const protocol = window.location.protocol === 'https:' ? 'https:' : 'http:';
-    const adsElem = document.getElementById(`vk_ads_${id}`);
-    const scriptElem = document.createElement('script');
-    scriptElem.type = 'text/javascript';
-    scriptElem.async = true;
-    scriptElem.src = `${protocol}//vk.com/js/api/openapi.js?152`;
-    adsElem.parentNode.insertBefore(scriptElem, adsElem.nextSibling);
+    vkAdLoad(id, adParams);
   }
 };
 
